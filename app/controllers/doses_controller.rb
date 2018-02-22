78c9ec091f1a3adbbad18885eb9a8e56674222ec
Alex Hughes <aphughes@talk21.com>
2018-02-22 17:28:11 +0000

    Added some very basic styling

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -18,7 +18,8 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path(params[:cocktail_id])
+    # render '../cocktails/index'
+    redirect_to cocktails_path
   end
 
   private
